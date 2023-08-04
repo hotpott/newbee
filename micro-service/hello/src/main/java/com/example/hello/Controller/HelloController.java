9438b098ce51140d6c9f2c75387507e7967ec26c
gaoyd-a <gaoyd-a@glodon.com>
2023-08-04 09:30:27 +0800

    hello服务转移到hello_module里面

---
+++ micro-service/hello/src/main/java/com/example/hello/Controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.hello.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ *
+ * @author gaoyd-a
+ * @date 2023/7/31
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
